4f8b4329ba13a21f947615fe9a8a558c813ed9ac
Damocle21 <d.molina.cl@gmail.com>
2016-11-27 12:23:48 +0100

    Update BuildDatabase.py

--- sessio-4/build_database/BuildDatabase.py
+++ sessio-4/build_database/BuildDatabase.py
@@ -2,7 +2,7 @@
 import os 
 
 def build_database (indir,outdir): #definim la funció
-    images = os.listdir(indir)  #llegeixo tots els noms dels fitxers del directori d'entrada i els guarda a images
+    images = os.listdir(indir)  #llegeixo tots els noms dels fitxers del directori d'entrada i els guarda a imatges
     outfile = open(outdir+'/ID.txt','w')   #crea el fitxer on escriu els id.
     for file in images: #recorre tots els id i els escriu al fitxer
         outfile.write(file[0:-4]+"\n") 
