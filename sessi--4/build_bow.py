7d8dbae59d36c43498fc2d3c9e4fe389b60add0c
Damocle21 <d.molina.cl@gmail.com>
2016-11-29 18:44:55 +0100

    Update build_bow.py

--- sessi--4/build_bow.py
+++ sessi--4/build_bow.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn import preprocessing
 
 def bow(assignments,kMeans):
-    # Creem un descriptor del mateix tamany que el número de clusters. Aquest descriptor està tot 
+    # Creem un descriptor de la mateixa mida que el número de clusters. Aquest descriptor està tot 
     #valor a cero
     descriptor = np.zeros(np.shape(kMeans.cluster_centers_)[0])
     # Per cada entrada a l'assigments, sumem 1 al índex que pertoca a l'histograma
