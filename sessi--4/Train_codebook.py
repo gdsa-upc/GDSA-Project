6657b3b426ac35f6cfb13f312e6da9cab9a7750c
MartaCollPol <maly-cat@hotmail.es>
2016-11-30 00:30:20 +0100

    Update Train_codebook.py

--- sessi--4/Train_codebook.py
+++ sessi--4/Train_codebook.py
@@ -3,6 +3,6 @@ from sklearn.cluster import MiniBatchKMeans
 def train_codebook(params,descriptors,paraules):
     kMeans = MiniBatchKMeans(params['descriptor_size'])
     #Entrenem el KMeans amb els descriptors
-    kMeans.fit(descriptors)
+    kMeans.fit(descriptors) #funció que calcula els centroids agrupant-los en mini lots.
     return kMeans
     
