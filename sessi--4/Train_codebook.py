c7ecaea1b2bb42c99e4e30b6291dfcd4d4c27328
Damocle21 <d.molina.cl@gmail.com>
2016-11-29 01:07:41 +0100

    Update Train_codebook.py

--- sessi--4/Train_codebook.py
+++ sessi--4/Train_codebook.py
@@ -2,7 +2,7 @@ from sklearn.cluster import MiniBatchKMeans
 
 def train_codebook(n_clusters,descriptors,paraules):
     kMeans = MiniBatchKMeans(paraules)
-    #Entrenem el KMeans, ajustem el numero de clusters a 4
+    #Entrenem el KMeans
     kMeans.fit(descriptores)
     return km
     
