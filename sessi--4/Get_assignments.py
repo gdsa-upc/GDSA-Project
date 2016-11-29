f1c5e19e9ef3d89c6050d22abdf94afda19fd22e
MartaCollPol <maly-cat@hotmail.es>
2016-11-29 23:26:03 +0100

    Update Get_assignments.py

--- sessi--4/Get_assignments.py
+++ sessi--4/Get_assignments.py
@@ -1,3 +1,5 @@
+#Amb aquesta funció cada descriptor local (obtinguts anteriorment) s'assigna al cluster que li correspon (per a fer-ho es mira quin
+#és el centroid més pròxim).
 def get_assignments (KMeans, descriptors):
     
     #Calculem les assignacions mitjancant la funcio predict( )
