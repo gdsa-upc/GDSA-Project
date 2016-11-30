2ae85ee10db38d7870f502a0278b176837d805e2
MartaCollPol <maly-cat@hotmail.es>
2016-11-30 01:10:15 +0100

    Update build_bow.py

--- sessi--4/build_bow.py
+++ sessi--4/build_bow.py
@@ -2,13 +2,14 @@
 import numpy as np
 from sklearn import preprocessing
 
-def bow(assignments,kMeans):
-    # Creem un descriptor de la mateixa mida que el número de clusters. Aquest descriptor està tot 
-    #valor a cero
-    descriptor = np.zeros(np.shape(kMeans.cluster_centers_)[0])
-    # Per cada entrada a l'assigments, sumem 1 al índex que pertoca a l'histograma
+#assignments és la sortida de la funció get_assignments que assigna un index de cluster a cada descriptor.
+def build_bow(assignments,kMeans): #kmeans és la sortida de la funció train_codebook (assigna el centroid a un descriptor)
+    # Definim el vector "l'histograma" bag of words per a que tingui la mateixa mida que el numero de clusters que tinguem.
+    bow = np.zeros(np.shape(kMeans.cluster_centers_)[0]) #cluster_centers_ és una matriu [n_clusters, n_features]
+    # Assigments retorna un vector amb l'idex de cluster assignat a cada descriptor.
+    # Construim un histograma sumant +1 per a cada descriptor assignat a un index de cluster.
     for a in assignments:
-        descriptor[a] += 1
-    # És important normalitzar amb L2 
-    descriptor = preprocessing.normalize(descriptor)
-    return descriptor
+        bow[a] += 1
+    # És important normalitzar amb L2
+    bow = preprocessing.normalize(bow)
+    return bow
