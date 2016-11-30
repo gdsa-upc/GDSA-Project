e233695038239714e3e9b540a5e2eb0c89519e27
MartaCollPol <maly-cat@hotmail.es>
2016-11-30 01:12:33 +0100

    Update Get_assignments.py

--- sessi--4/Get_assignments.py
+++ sessi--4/Get_assignments.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #Amb aquesta funció cada descriptor local (obtinguts anteriorment) s'assigna al cluster que li correspon (per a fer-ho es mira quin
 #és el centroid més pròxim).  Cal recordar que descriptors és una matriu amb forma=(n_samples,n_features)
 def get_assignments (KMeans, descriptors): #kmeans és la sortida del codebook
